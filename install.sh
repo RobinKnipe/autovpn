185da68d521ff0603991aeddcab5973cc320bf8f
Robin Knipe <robin.knipe@gmail.com>
2019-01-15 21:52:34 +0000

    Fix the missing user home directory problem
    ...when running the script as `root`, by specifying the `$HOME`
    directory when the install script is run.

--- install.sh
+++ install.sh
@@ -6,6 +6,12 @@ if [ `whoami` != "root" ] ; then
   exit 1
 fi
 
+# check the user home dir is specified
+if [ ! -d "$1" ] ; then
+  echo "ERROR: the script must be called with one parameter, the location of the user's home directory"
+  exit 2
+fi
+
 # Install autovpn
 
 user_dir="$1"
@@ -22,6 +28,15 @@ function install_prerequisits {
   fi
 }
 
+# setup the properties file
+function install_properties {
+  mkdir -p "${user_dir}/.config"
+  props="${user_dir}/.config/autovpn.properties"
+  echo "# the location of the user's downloads folder monitored by autovpn" > ${props}
+  echo "download_dir=${user_dir}/Downloads" >> ${props}
+  chown `ls -ld ${user_dir}/ | awk '{print $3":"$4}'` "${user_dir}/.config" "${props}"
+}
+
 # download the main autovpn script file
 function install_main_script {
   echo 'Installing the main autovpn script file'
@@ -31,9 +46,20 @@ function install_main_script {
 }
 
 # download and activate the autovpn systemd unit
-function install_unit_file {
+function install_unit_files {
+  # stop the service if it is already installed
+  if [ -f "${unit_dir}/autovpn.service" ] ; then
+    systemctl stop autovpn.service
+    systemctl disable autovpn.service
+    systemctl daemon-reload
+  fi
+
   echo 'Installing the autovpn systemd unit file'
-  mkdir -p ${unit_dir}
+  mkdir -p "${unit_dir}/autovpn.service.d"
+  unit_env="${unit_dir}/autovpn.service.d/env.conf"
+  echo "[Service]" > ${unit_env}
+  echo "Environment=\"USER_HOME=${user_dir}\"" >> ${unit_env}
+
   curl -so "${unit_dir}/autovpn.service" "${repo}/autovpn.service"
   systemctl daemon-reload
   systemctl start autovpn.service
@@ -41,5 +67,6 @@ function install_unit_file {
 }
 
 install_prerequisits
+install_properties
 install_main_script
-install_unit_file
+install_unit_files
